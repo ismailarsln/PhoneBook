c36159805aeff6275b98f8858f8e2299c7873172
Ismail Arslan <58330742+ismailarsln@users.noreply.github.com>
2022-08-05 16:32:57 +0300

    Proje dosyası ekle.

---
+++ phoneContact/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace phoneContact
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add_btn = new System.Windows.Forms.Button();
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.id_btn = new System.Windows.Forms.Label();
+            this.name_btn = new System.Windows.Forms.Label();
+            this.last_btn = new System.Windows.Forms.Label();
+            this.phone_btn = new System.Windows.Forms.Label();
+            this.id_lbl = new System.Windows.Forms.TextBox();
+            this.name_lbl = new System.Windows.Forms.TextBox();
+            this.last_lbl = new System.Windows.Forms.TextBox();
+            this.phone_lbl = new System.Windows.Forms.TextBox();
+            this.search_btn = new System.Windows.Forms.Label();
+            this.search_lbl = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.srch_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // add_btn
+            // 
+            this.add_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.add_btn.Location = new System.Drawing.Point(408, 12);
+            this.add_btn.Name = "add_btn";
+            this.add_btn.Size = new System.Drawing.Size(150, 68);
+            this.add_btn.TabIndex = 0;
+            this.add_btn.Text = "Add!";
+            this.add_btn.UseVisualStyleBackColor = false;
+            this.add_btn.Click += new System.EventHandler(this.add_btn_Click);
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.remove_btn.Location = new System.Drawing.Point(408, 86);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(150, 68);
+            this.remove_btn.TabIndex = 1;
+            this.remove_btn.Text = "Remove!";
+            this.remove_btn.UseVisualStyleBackColor = false;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_Click);
+            // 
+            // update_btn
+            // 
+            this.update_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.update_btn.Location = new System.Drawing.Point(408, 160);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(150, 68);
+            this.update_btn.TabIndex = 2;
+            this.update_btn.Text = "Update!";
+            this.update_btn.UseVisualStyleBackColor = false;
+            this.update_btn.Click += new System.EventHandler(this.update_btn_Click);
+            // 
+            // id_btn
+            // 
+            this.id_btn.AutoSize = true;
+            this.id_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.id_btn.Location = new System.Drawing.Point(89, 19);
+            this.id_btn.Name = "id_btn";
+            this.id_btn.Size = new System.Drawing.Size(28, 17);
+            this.id_btn.TabIndex = 3;
+            this.id_btn.Text = "ID:";
+            // 
+            // name_btn
+            // 
+            this.name_btn.AutoSize = true;
+            this.name_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.name_btn.Location = new System.Drawing.Point(61, 48);
+            this.name_btn.Name = "name_btn";
+            this.name_btn.Size = new System.Drawing.Size(56, 17);
+            this.name_btn.TabIndex = 4;
+            this.name_btn.Text = "NAME:";
+            // 
+            // last_btn
+            // 
+            this.last_btn.AutoSize = true;
+            this.last_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.last_btn.Location = new System.Drawing.Point(22, 81);
+            this.last_btn.Name = "last_btn";
+            this.last_btn.Size = new System.Drawing.Size(95, 17);
+            this.last_btn.TabIndex = 5;
+            this.last_btn.Text = "LASTNAME:";
+            // 
+            // phone_btn
+            // 
+            this.phone_btn.AutoSize = true;
+            this.phone_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.phone_btn.Location = new System.Drawing.Point(50, 115);
+            this.phone_btn.Name = "phone_btn";
+            this.phone_btn.Size = new System.Drawing.Size(67, 17);
+            this.phone_btn.TabIndex = 6;
+            this.phone_btn.Text = "PHONE:";
+            // 
+            // id_lbl
+            // 
+            this.id_lbl.Location = new System.Drawing.Point(123, 14);
+            this.id_lbl.Name = "id_lbl";
+            this.id_lbl.Size = new System.Drawing.Size(170, 22);
+            this.id_lbl.TabIndex = 7;
+            // 
+            // name_lbl
+            // 
+            this.name_lbl.Location = new System.Drawing.Point(123, 45);
+            this.name_lbl.Name = "name_lbl";
+            this.name_lbl.Size = new System.Drawing.Size(170, 22);
+            this.name_lbl.TabIndex = 8;
+            // 
+            // last_lbl
+            // 
+            this.last_lbl.Location = new System.Drawing.Point(123, 78);
+            this.last_lbl.Name = "last_lbl";
+            this.last_lbl.Size = new System.Drawing.Size(170, 22);
+            this.last_lbl.TabIndex = 9;
+            // 
+            // phone_lbl
+            // 
+            this.phone_lbl.Location = new System.Drawing.Point(123, 110);
+            this.phone_lbl.Name = "phone_lbl";
+            this.phone_lbl.Size = new System.Drawing.Size(170, 22);
+            this.phone_lbl.TabIndex = 10;
+            // 
+            // search_btn
+            // 
+            this.search_btn.AutoSize = true;
+            this.search_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.search_btn.Location = new System.Drawing.Point(22, 187);
+            this.search_btn.Name = "search_btn";
+            this.search_btn.Size = new System.Drawing.Size(120, 17);
+            this.search_btn.TabIndex = 11;
+            this.search_btn.Text = "SEARCH BY ID:";
+            // 
+            // search_lbl
+            // 
+            this.search_lbl.Location = new System.Drawing.Point(143, 181);
+            this.search_lbl.Name = "search_lbl";
+            this.search_lbl.Size = new System.Drawing.Size(65, 22);
+            this.search_lbl.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 234);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(533, 307);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // srch_btn
+            // 
+            this.srch_btn.Location = new System.Drawing.Point(214, 180);
+            this.srch_btn.Name = "srch_btn";
+            this.srch_btn.Size = new System.Drawing.Size(67, 23);
+            this.srch_btn.TabIndex = 14;
+            this.srch_btn.Text = "Search!";
+            this.srch_btn.UseVisualStyleBackColor = true;
+            this.srch_btn.Click += new System.EventHandler(this.srch_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.srch_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.search_lbl);
+            this.Controls.Add(this.search_btn);
+            this.Controls.Add(this.phone_lbl);
+            this.Controls.Add(this.last_lbl);
+            this.Controls.Add(this.name_lbl);
+            this.Controls.Add(this.id_lbl);
+            this.Controls.Add(this.phone_btn);
+            this.Controls.Add(this.last_btn);
+            this.Controls.Add(this.name_btn);
+            this.Controls.Add(this.id_btn);
+            this.Controls.Add(this.update_btn);
+            this.Controls.Add(this.remove_btn);
+            this.Controls.Add(this.add_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phone Book";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add_btn;
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.Label id_btn;
+        private System.Windows.Forms.Label name_btn;
+        private System.Windows.Forms.Label last_btn;
+        private System.Windows.Forms.Label phone_btn;
+        private System.Windows.Forms.TextBox id_lbl;
+        private System.Windows.Forms.TextBox name_lbl;
+        private System.Windows.Forms.TextBox last_lbl;
+        private System.Windows.Forms.TextBox phone_lbl;
+        private System.Windows.Forms.Label search_btn;
+        private System.Windows.Forms.TextBox search_lbl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button srch_btn;
+    }
+}
---
+++ phoneContact/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace phoneContact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string conString =
+            "server name...;" +
+            "database name...;" + 
+            "Trusted_Connection = True;";
+
+        SqlConnection connect = new SqlConnection(conString);
+
+        private void getData()
+        {
+            connect.Open();
+
+            string reg = "SELECT * from tblUsers"; 
+
+            SqlCommand command = new SqlCommand(reg, connect);
+
+            SqlDataAdapter da = new SqlDataAdapter(command);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            connect.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+        private void srch_btn_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            string reg = "SELECT * from tblUsers where PersonID=@PersonID";
+            SqlCommand command = new SqlCommand(reg, connect);
+            command.Parameters.AddWithValue("@PersonID", search_lbl.Text);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connect.Close();
+        }
+
+        private void update_btn_Click(object sender, EventArgs e)
+        {
+            string reg = 
+                "Update tblUsers Set FirstName=@FirstName," +
+                "LastName=@LastName," +
+                "Phone=@Phone Where PersonID=@PersonID";
+            SqlCommand command = new SqlCommand(reg, connect);
+            command.Parameters.AddWithValue("@PersonID", Convert.ToInt32(id_lbl.Text));
+            command.Parameters.AddWithValue("@FirstName", name_lbl.Text);
+            command.Parameters.AddWithValue("@LastName", last_lbl.Text);
+            command.Parameters.AddWithValue("@Phone", phone_lbl.Text);
+
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+            getData();
+        }
+
+        private void add_btn_Click(object sender, EventArgs e)
+        {
+            string reg = "Insert into tblUsers " +
+                "(FirstName,LastName,Phone) values " +
+                "(@FirstName,@LastName,@Phone)";
+            SqlCommand command = new SqlCommand(reg, connect);
+            command.Parameters.AddWithValue("@FirstName", name_lbl.Text);
+            command.Parameters.AddWithValue("@LastName", last_lbl.Text);
+            command.Parameters.AddWithValue("@Phone", phone_lbl.Text);
+
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+            getData();
+        }
+
+        private void remove_btn_Click(object sender, EventArgs e)
+        {
+            string reg = "Delete From tblUsers Where PersonID=@PersonID";
+            SqlCommand command = new SqlCommand(reg, connect);
+            command.Parameters.AddWithValue("@PersonID", Convert.ToInt32(id_lbl.Text));
+
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+            getData();
+        }
+    }
+}
